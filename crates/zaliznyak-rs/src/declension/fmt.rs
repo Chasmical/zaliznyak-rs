cc46fc35dee3d2594d9c3a37209f20017d4b406b
Chasmical <kaz@chsm.dev>
2025-08-19 16:05:25 +0700

    ✅ Add formatting and parsing tests

--- crates/zaliznyak-rs/src/declension/fmt.rs
+++ crates/zaliznyak-rs/src/declension/fmt.rs
@@ -80,7 +80,7 @@ impl PronounDeclension {
 }
 impl AdjectiveDeclension {
     pub const fn fmt_to(self, dst: &mut [u8; DECLENSION_MAX_LEN]) -> &mut str {
-        fmt_declension_any(dst, self.stem_type.into(), self.flags, self.stress.into())
+        fmt_declension_any(dst, self.stem_type.into(), self.flags, self.stress.abbr())
     }
 }
 impl Declension {
@@ -98,7 +98,7 @@ impl Declension {
             },
             Self::Adjective(decl) => {
                 dst.push_str("п ");
-                (decl.stem_type.into(), decl.flags, decl.stress.into())
+                (decl.stem_type.into(), decl.flags, decl.stress.abbr())
             },
         };
 
@@ -132,5 +132,81 @@ impl std::fmt::Display for AdjectiveDeclension {
 impl std::fmt::Display for Declension {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         self.fmt_to(&mut [0; _]).fmt(f)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{DeclensionFlags as DF, *};
+    use crate::{
+        declension::{AdjectiveStemType, NounStemType, PronounStemType},
+        stress::{AdjectiveStress, NounStress, PronounStress},
+    };
+
+    #[test]
+    fn fmt_flags() {
+        assert_eq!(DF::empty().to_string(), "");
+        assert_eq!(DF::STAR.to_string(), "*");
+        assert_eq!(DF::CIRCLE.to_string(), "°");
+        assert_eq!(DF::CIRCLED_ONE.to_string(), "①");
+        assert_eq!(DF::CIRCLED_TWO.to_string(), "②");
+        assert_eq!(DF::CIRCLED_THREE.to_string(), "③");
+        assert_eq!(DF::ALTERNATING_YO.to_string(), ", ё");
+        assert_eq!((DF::CIRCLE | DF::STAR).to_string(), "°*");
+        assert_eq!((DF::CIRCLE | DF::CIRCLED_ONE | DF::ALTERNATING_YO).to_string(), "°①, ё");
+        assert_eq!(DF::all().to_string(), "°*①②③, ё");
+    }
+
+    #[test]
+    fn fmt_noun() {
+        use {NounStemType::*, NounStress::*};
+
+        let assert_fmt = |stem_type, stress, flags, expected: &str| {
+            assert_eq!(NounDeclension { stem_type, stress, flags }.to_string(), expected);
+        };
+
+        assert_fmt(Type1, A, DF::empty(), "1a");
+        assert_fmt(Type2, Bp, DF::empty(), "2b′");
+        assert_fmt(Type3, F, DF::CIRCLE, "3°f");
+        assert_fmt(Type4, Fp, DF::STAR, "4*f′");
+        assert_fmt(Type5, Fpp, DF::CIRCLE | DF::STAR, "5°*f″");
+        assert_fmt(Type6, C, DF::STAR | DF::ALTERNATING_YO, "6*c, ё");
+        assert_fmt(Type7, Dp, DF::STAR | DF::CIRCLED_ONE | DF::ALTERNATING_YO, "7*d′①, ё");
+        assert_fmt(Type8, E, DF::CIRCLED_ONE | DF::CIRCLED_TWO | DF::CIRCLED_THREE, "8e①②③");
+        assert_fmt(Type8, Fpp, DF::all(), "8°*f″①②③, ё");
+    }
+
+    #[test]
+    fn fmt_pronoun() {
+        use {PronounStemType::*, PronounStress::*};
+
+        let assert_fmt = |stem_type, stress, flags, expected: &str| {
+            assert_eq!(PronounDeclension { stem_type, stress, flags }.to_string(), expected);
+        };
+
+        assert_fmt(Type1, A, DF::empty(), "1a");
+        assert_fmt(Type2, B, DF::STAR, "2*b");
+        assert_fmt(Type4, F, DF::STAR | DF::CIRCLED_ONE | DF::CIRCLED_TWO, "4*f①②");
+        assert_fmt(Type6, B, DF::CIRCLE | DF::STAR | DF::ALTERNATING_YO, "6°*b, ё");
+        assert_fmt(Type6, F, DF::all(), "6°*f①②③, ё");
+    }
+
+    #[test]
+    fn fmt_adjective() {
+        use {AdjectiveStemType::*, AdjectiveStress as S};
+
+        let assert_fmt = |stem_type, stress, flags, expected: &str| {
+            assert_eq!(AdjectiveDeclension { stem_type, stress, flags }.to_string(), expected);
+        };
+
+        assert_fmt(Type1, S::A_A, DF::empty(), "1a");
+        assert_fmt(Type2, S::A_B, DF::empty(), "2a/b");
+        assert_fmt(Type3, S::B_B, DF::STAR, "3*b");
+        assert_fmt(Type4, S::B_C, DF::CIRCLE, "4°b/c");
+        assert_fmt(Type5, S::A_Ap, DF::CIRCLED_ONE | DF::CIRCLED_THREE, "5a′①③");
+        assert_fmt(Type6, S::B_Bp, DF::CIRCLE | DF::STAR | DF::ALTERNATING_YO, "6°*b′, ё");
+        assert_fmt(Type7, S::B_Cp, DF::CIRCLED_TWO | DF::CIRCLED_THREE, "7b/c′②③");
+        assert_fmt(Type1, S::A_Cpp, DF::STAR | DF::CIRCLED_ONE, "1*a/c″①");
+        assert_fmt(Type7, S::B_Cpp, DF::all(), "7°*b/c″①②③, ё");
     }
 }
--- crates/zaliznyak-rs/src/declension/from_str.rs
+++ crates/zaliznyak-rs/src/declension/from_str.rs
@@ -193,3 +193,131 @@ impl std::str::FromStr for Declension {
         Self::from_str_or_err(s, Self::Err::Invalid)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{DeclensionFlags as DF, *};
+    use crate::{
+        declension::{AdjectiveStemType, NounStemType, PronounStemType},
+        stress::{AdjectiveStress, NounStress, PronounStress},
+    };
+
+    #[test]
+    fn parse_flags() {
+        let assert_ok = |s: &str, flags| assert_eq!(s.parse(), Ok(flags));
+        let assert_err = |s: &str, err| assert_eq!(s.parse::<DeclensionFlags>(), Err(err));
+
+        assert_ok("", DF::empty());
+        assert_ok("°", DF::CIRCLE);
+        assert_ok("*", DF::STAR);
+        assert_ok("(1)", DF::CIRCLED_ONE);
+        assert_ok("(1)②", DF::CIRCLED_ONE | DF::CIRCLED_TWO);
+        assert_ok("①(2)③", DF::CIRCLED_ONE | DF::CIRCLED_TWO | DF::CIRCLED_THREE);
+        assert_ok("°②(3)", DF::CIRCLE | DF::CIRCLED_TWO | DF::CIRCLED_THREE);
+        assert_ok(", ё", DF::ALTERNATING_YO);
+        assert_ok("°, ё", DF::CIRCLE | DF::ALTERNATING_YO);
+        assert_ok("°*①②③, ё", DF::all());
+        assert_ok("°*(1)②(3), ё", DF::all());
+
+        assert_err("(1)(1)", Error::InvalidFlags);
+        assert_err("②②", Error::InvalidFlags);
+        assert_err("(2)(1)", Error::InvalidFlags);
+        assert_err("(3)(2)", Error::InvalidFlags);
+        assert_err("(1)(2)(1)", Error::InvalidFlags);
+        assert_err("①②(1)", Error::InvalidFlags);
+    }
+
+    #[test]
+    fn parse_noun() {
+        use {NounStemType::*, NounStress::*};
+
+        let assert_ok = |s: &str, stem_type, stress, flags| {
+            assert_eq!(s.parse(), Ok(NounDeclension { stem_type, stress, flags }))
+        };
+        let assert_err = |s: &str, err| assert_eq!(s.parse::<NounDeclension>(), Err(err));
+
+        assert_ok("1b", Type1, B, DF::empty());
+        assert_ok("2a", Type2, A, DF::empty());
+        assert_ok("3°d′", Type3, Dp, DF::CIRCLE);
+        assert_ok("4*f", Type4, F, DF::STAR);
+        assert_ok("5f'(1)", Type5, Fp, DF::CIRCLED_ONE);
+        assert_ok("6e②(3)", Type6, E, DF::CIRCLED_TWO | DF::CIRCLED_THREE);
+        assert_ok("7f\"(2), ё", Type7, Fpp, DF::CIRCLED_TWO | DF::ALTERNATING_YO);
+        assert_ok("8°b′③", Type8, Bp, DF::CIRCLE | DF::CIRCLED_THREE);
+        assert_ok("8°*f″①(2)③, ё", Type8, Fpp, DF::all());
+
+        assert_err("", Error::InvalidStemType);
+        assert_err("0", Error::InvalidStemType);
+        assert_err("9", Error::InvalidStemType);
+        assert_err("z", Error::InvalidStemType);
+        assert_err("4", Error::InvalidStress(ParseStressError::InvalidLetter));
+        assert_err("4z", Error::InvalidStress(ParseStressError::InvalidLetter));
+        assert_err("42", Error::InvalidStress(ParseStressError::InvalidLetter));
+        assert_err("4b″", Error::InvalidStress(ParseStressError::InvalidPrime));
+        assert_err("4a′", Error::IncompatibleStress);
+        assert_err("4a/a", Error::IncompatibleStress);
+    }
+
+    #[test]
+    fn parse_pronoun() {
+        use {PronounStemType::*, PronounStress::*};
+
+        let assert_ok = |s: &str, stem_type, stress, flags| {
+            assert_eq!(s.parse(), Ok(PronounDeclension { stem_type, stress, flags }))
+        };
+        let assert_err = |s: &str, err| assert_eq!(s.parse::<PronounDeclension>(), Err(err));
+
+        assert_ok("1a", Type1, A, DF::empty());
+        assert_ok("2f(1)", Type2, F, DF::CIRCLED_ONE);
+        assert_ok("4°*b", Type4, B, DF::CIRCLE | DF::STAR);
+        assert_ok("6°*f①②(3), ё", Type6, F, DF::all());
+
+        assert_err("2c", Error::IncompatibleStress);
+        assert_err("2a/a", Error::IncompatibleStress);
+    }
+
+    #[test]
+    fn parse_adjective() {
+        use {AdjectiveStemType::*, AdjectiveStress as S};
+
+        let assert_ok = |s: &str, stem_type, stress, flags| {
+            assert_eq!(s.parse(), Ok(AdjectiveDeclension { stem_type, stress, flags }))
+        };
+        let assert_err = |s: &str, err| assert_eq!(s.parse::<AdjectiveDeclension>(), Err(err));
+
+        assert_ok("1b/b", Type1, S::B_B, DF::empty());
+        assert_ok("2°a", Type2, S::A_A, DF::CIRCLE);
+        assert_ok("3*b/a", Type3, S::B_A, DF::STAR);
+        assert_ok("4b(1)②", Type4, S::B_B, DF::CIRCLED_ONE | DF::CIRCLED_TWO);
+        assert_ok("5b/c(1), ё", Type5, S::B_C, DF::CIRCLED_ONE | DF::ALTERNATING_YO);
+        assert_ok("6a/c′②", Type6, S::A_Cp, DF::CIRCLED_TWO);
+        assert_ok("7°*b/c''(1)(2)③, ё", Type7, S::B_Cpp, DF::all());
+
+        assert_err("2c", Error::IncompatibleStress);
+        assert_err("2a/f", Error::IncompatibleStress);
+    }
+
+    #[test]
+    fn parse_declension() {
+        let decl = Declension::Noun(NounDeclension {
+            stem_type: NounStemType::Type3,
+            stress: NounStress::Bp,
+            flags: DeclensionFlags::CIRCLE | DeclensionFlags::ALTERNATING_YO,
+        });
+        assert_eq!("3°b′, ё".parse(), Ok(decl));
+
+        let decl = Declension::Pronoun(PronounDeclension {
+            stem_type: PronounStemType::Type6,
+            stress: PronounStress::F,
+            flags: DeclensionFlags::STAR | DeclensionFlags::CIRCLED_ONE,
+        });
+        assert_eq!("мс 6*f(1)".parse(), Ok(decl));
+
+        let decl = Declension::Adjective(AdjectiveDeclension {
+            stem_type: AdjectiveStemType::Type4,
+            stress: AdjectiveStress::B_Ap,
+            flags: DeclensionFlags::STAR | DeclensionFlags::CIRCLED_TWO,
+        });
+        assert_eq!("п 4*b/a′(2)".parse(), Ok(decl));
+    }
+}

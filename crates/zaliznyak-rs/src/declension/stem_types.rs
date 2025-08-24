362e3db52555b4852b0233c9c3e78bca71e7c51e
Chasmical <kaz@chsm.dev>
2025-08-24 20:55:58 +0700

    ✨ Add some basic stem identification methods

--- crates/zaliznyak-rs/src/declension/stem_types.rs
+++ crates/zaliznyak-rs/src/declension/stem_types.rs
@@ -1,4 +1,4 @@
-use crate::util::enum_conversion;
+use crate::{alphabet::Letter, util::enum_conversion};
 use thiserror::Error;
 
 macro_rules! impl_stem_type {
@@ -112,4 +112,133 @@ enum_conversion! {
 enum_conversion! {
     AdjectiveStemType => AnyStemType { Type1, Type2, Type3, Type4, Type5, Type6, Type7 }
     else { AdjectiveStemTypeError }
+}
+
+fn is_trim_letter(letter: Letter) -> bool {
+    use Letter::*;
+    matches!(letter, А | Е | И | Й | О | У | Ы | Ь | Э | Ю | Я | Ё)
+}
+fn identify_stem_type(stem: Letter, after: Option<Letter>) -> Option<AdjectiveStemType> {
+    use {AdjectiveStemType as StemType, Letter::*};
+
+    Some(match stem {
+        Г | К | Х => StemType::Type3,
+        Ж | Ч | Ш | Щ => StemType::Type4,
+        Ц => StemType::Type5,
+        А | Е | Й | О | У | Ы | Ь | Э | Ю | Я | Ё => StemType::Type6,
+        И => StemType::Type7,
+        Б | В | Д | З | Л | М | Н | П | Р | С | Т | Ф => {
+            let hard = matches!(after, None | Some(А | О | У | Ы | Э));
+            if hard { StemType::Type1 } else { StemType::Type2 }
+        },
+        Ъ => return None,
+    })
+}
+
+impl NounStemType {
+    pub fn identify(word: &str) -> Option<(&str, NounStemType)> {
+        // Read the word's last char (must be in [а-яё] range)
+        let last = Letter::from_char(word.chars().last()?)?;
+
+        let (stem, stem_char, after) = {
+            // If the last char is trimmable, exclude it from stem
+            if is_trim_letter(last) {
+                let stem = unsafe { word.get_unchecked(0..(word.len() - 2)) };
+                // Read the actual last stem char (must be in [а-яё] range)
+                let stem_char = Letter::from_char(stem.chars().last()?)?;
+                (stem, stem_char, Some(last))
+            } else {
+                (word, last, None)
+            }
+        };
+
+        // Identify the stem type from letters
+        let stem_type = identify_stem_type(stem_char, after)?;
+        Some((stem, AnyStemType::from(stem_type).into()))
+    }
+}
+
+impl PronounStemType {
+    pub fn identify(word: &str) -> Option<(&str, PronounStemType)> {
+        let (stem, stem_type) = NounStemType::identify(word)?;
+        Some((stem, AnyStemType::from(stem_type).try_into().ok()?))
+    }
+}
+
+impl AdjectiveStemType {
+    pub fn identify(word: &str) -> Option<(&str, AdjectiveStemType, bool)> {
+        let (word, is_reflexive) = {
+            // Remove 'ся' suffix from reflexive adjectives
+            word.strip_suffix("ся").map_or((word, false), |x| (x, true))
+        };
+
+        let mut iter = word.chars().rev();
+
+        // Read the word's two ending chars (both must be in [а-яё] range)
+        _/*let ending_last_char*/ = Letter::from_char(iter.next()?)?;
+        let ending_first_char = Letter::from_char(iter.next()?)?;
+
+        // Read the stem's last char (must be in [а-яё] range)
+        let stem_char = Letter::from_char(iter.next()?)?;
+
+        // ending is always 4 bytes (2 chars) long, so slicing is safe
+        let stem = unsafe { word.get_unchecked(..(word.len() - 4)) };
+
+        // Identify the stem type from letters
+        let stem_type = identify_stem_type(stem_char, Some(ending_first_char))?;
+        Some((stem, stem_type, is_reflexive))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn identify_noun() {
+        use NounStemType as ST;
+
+        assert_eq!(ST::identify("ведро"), Some(("ведр", ST::Type1)));
+        assert_eq!(ST::identify("тополь"), Some(("топол", ST::Type2)));
+        assert_eq!(ST::identify("сапог"), Some(("сапог", ST::Type3)));
+        assert_eq!(ST::identify("дача"), Some(("дач", ST::Type4)));
+        assert_eq!(ST::identify("яйцо"), Some(("яйц", ST::Type5)));
+        assert_eq!(ST::identify("бельё"), Some(("бель", ST::Type6)));
+        assert_eq!(ST::identify("литий"), Some(("лити", ST::Type7)));
+
+        assert_eq!(ST::identify("воръ"), None);
+        assert_eq!(ST::identify("noun"), None);
+        assert_eq!(ST::identify("nounя"), None);
+    }
+
+    #[test]
+    fn identify_pro() {
+        use PronounStemType as ST;
+
+        assert_eq!(ST::identify("отцов"), Some(("отцов", ST::Type1)));
+        assert_eq!(ST::identify("господень"), Some(("господен", ST::Type2)));
+        assert_eq!(ST::identify("наш"), Some(("наш", ST::Type4)));
+        assert_eq!(ST::identify("твой"), Some(("тво", ST::Type6)));
+
+        assert_eq!(ST::identify("сёмга"), None); // stem type 3 - incompatible
+        assert_eq!(ST::identify("блюдце"), None); // stem type 5 - incompatible
+        assert_eq!(ST::identify("усилие"), None); // stem type 7 - incompatible
+    }
+
+    #[test]
+    fn identify_adj() {
+        use AdjectiveStemType as ST;
+
+        assert_eq!(ST::identify("живой"), Some(("жив", ST::Type1, false)));
+        assert_eq!(ST::identify("осенний"), Some(("осенн", ST::Type2, false)));
+        assert_eq!(ST::identify("плавкий"), Some(("плавк", ST::Type3, false)));
+        assert_eq!(ST::identify("светящийся"), Some(("светящ", ST::Type4, true)));
+        assert_eq!(ST::identify("куцый"), Some(("куц", ST::Type5, false)));
+        assert_eq!(ST::identify("голошеий"), Some(("голоше", ST::Type6, false)));
+        assert_eq!(ST::identify("сиой"), Some(("си", ST::Type7, false))); // not a real adjective
+
+        assert_eq!(ST::identify("серъое"), None);
+        assert_eq!(ST::identify("adjective"), None);
+        assert_eq!(ST::identify("adjectiveое"), None);
+    }
 }

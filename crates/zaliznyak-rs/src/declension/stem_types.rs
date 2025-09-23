f38d7def138c4eae740b5c7281b7e031e5718d76
Chasmical <kaz@chsm.dev>
2025-09-23 21:47:26 +0700

    📝 Add docs to stem type enums

--- crates/zaliznyak-rs/src/declension/stem_types.rs
+++ crates/zaliznyak-rs/src/declension/stem_types.rs
@@ -53,50 +53,65 @@ macro_rules! impl_stem_type {
     );
 }
 
+/// Error type for parsing various stem types.
 #[derive(Debug, Error, Copy, Eq, Hash)]
 #[derive_const(Clone, PartialEq)]
 pub enum ParseStemTypeError {
+    /// The parsed value is not compatible with specified stem type.
     #[error("digit not compatible with specified type")]
     IncompatibleDigit,
+    /// Invalid format.
     #[error("invalid format")]
     Invalid,
 }
 
+/// Error type for conversion to [`AnyStemType`].
 #[derive(Debug, Error, Copy, Eq, Hash)]
 #[derive_const(Default, Clone, PartialEq)]
 #[error("words can only have stem types 1 through 8")]
 pub struct AnyStemTypeError;
+/// Error type for conversion to [`NounStemType`].
 #[derive(Debug, Error, Copy, Eq, Hash)]
 #[derive_const(Default, Clone, PartialEq)]
 #[error("nouns can only have stem types 1 through 8")]
 pub struct NounStemTypeError;
+/// Error type for conversion to [`PronounStemType`].
 #[derive(Debug, Error, Copy, Eq, Hash)]
 #[derive_const(Default, Clone, PartialEq)]
 #[error("pronouns can only have stem types 1, 2, 4 and 6")]
 pub struct PronounStemTypeError;
+/// Error type for conversion to [`AdjectiveStemType`].
 #[derive(Debug, Error, Copy, Eq, Hash)]
 #[derive_const(Default, Clone, PartialEq)]
 #[error("adjectives can only have stem types 1 through 7")]
 pub struct AdjectiveStemTypeError;
 
 impl_stem_type! {
+    /// Any word's stem type. Can be converted to and from any other stem type.
+    /// [See the dictionary for more details](https://gramdict.ru/declension/symbols#meaning1).
     pub enum AnyStemType {
         Type1 = 1, Type2 = 2, Type3 = 3, Type4 = 4,
         Type5 = 5, Type6 = 6, Type7 = 7, Type8 = 8,
     }
 }
 impl_stem_type! {
+    /// A non stem type.
+    /// [See the dictionary for more details](https://gramdict.ru/declension/symbols#meaning1).
     pub enum NounStemType {
         Type1 = 1, Type2 = 2, Type3 = 3, Type4 = 4,
         Type5 = 5, Type6 = 6, Type7 = 7, Type8 = 8,
     }
 }
 impl_stem_type! {
+    /// A pronoun stem type.
+    /// [See the dictionary for more details](https://gramdict.ru/declension/symbols#meaning1).
     pub enum PronounStemType {
         Type1 = 1, Type2 = 2, Type4 = 4, Type6 = 6,
     }
 }
 impl_stem_type! {
+    /// An adjective stem type.
+    /// [See the dictionary for more details](https://gramdict.ru/declension/symbols#meaning1).
     pub enum AdjectiveStemType {
         Type1 = 1, Type2 = 2, Type3 = 3, Type4 = 4,
         Type5 = 5, Type6 = 6, Type7 = 7,
@@ -139,6 +154,23 @@ const fn identify_stem_type(
 }
 
 impl NounStemType {
+    /// Identifies a noun's stem and stem type from its nominative form.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use zaliznyak::declension::NounStemType;
+    ///
+    /// assert_eq!(NounStemType::identify("акулы"), Some(("акул", NounStemType::Type1)));
+    /// assert_eq!(NounStemType::identify("точка"), Some(("точк", NounStemType::Type3)));
+    /// assert_eq!(NounStemType::identify("блюдца"), Some(("блюдц", NounStemType::Type5)));
+    /// assert_eq!(NounStemType::identify("литий"), Some(("лити", NounStemType::Type7)));
+    ///
+    /// assert_eq!(NounStemType::identify("циркъ"), None);
+    /// assert_eq!(NounStemType::identify("wxyz"), None);
+    /// assert_eq!(NounStemType::identify("ы"), None);
+    /// assert_eq!(NounStemType::identify(""), None);
+    /// ```
     pub const fn identify(word: &str) -> Option<(&str, NounStemType)> {
         // Read the word's last char
         let (word_without_last, last) = Utf8Letter::split_last(word)?;
@@ -161,6 +193,25 @@ impl NounStemType {
 }
 
 impl PronounStemType {
+    /// Identifies a pronoun's stem and stem type from its nominative form.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use zaliznyak::declension::PronounStemType;
+    ///
+    /// assert_eq!(PronounStemType::identify("один"), Some(("один", PronounStemType::Type1)));
+    /// assert_eq!(PronounStemType::identify("наши"), Some(("наш", PronounStemType::Type4)));
+    /// assert_eq!(PronounStemType::identify("своё"), Some(("сво", PronounStemType::Type6)));
+    ///
+    /// assert_eq!(PronounStemType::identify("точки"), None); // 3 - not compatible with pronouns
+    /// assert_eq!(PronounStemType::identify("солнце"), None); // 5 - not compatible with pronouns
+    /// assert_eq!(PronounStemType::identify("чий"), None); // 7 - not compatible with pronouns
+    ///
+    /// assert_eq!(PronounStemType::identify("wxyz"), None);
+    /// assert_eq!(PronounStemType::identify("ы"), None);
+    /// assert_eq!(PronounStemType::identify(""), None);
+    /// ```
     pub const fn identify(word: &str) -> Option<(&str, PronounStemType)> {
         let (stem, stem_type) = NounStemType::identify(word)?;
         Some((stem, AnyStemType::from(stem_type).try_into().ok()?))
@@ -168,6 +219,41 @@ impl PronounStemType {
 }
 
 impl AdjectiveStemType {
+    /// Identifies an adjective's stem and stem type from its nominative form, also returning a
+    /// `bool` indicating whether the adjective is reflexive or not.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use zaliznyak::declension::AdjectiveStemType;
+    ///
+    /// assert_eq!(
+    ///     AdjectiveStemType::identify("красный"),
+    ///     Some(("красн", AdjectiveStemType::Type1, false)),
+    /// );
+    /// assert_eq!(
+    ///     AdjectiveStemType::identify("мягкая"),
+    ///     Some(("мягк", AdjectiveStemType::Type3, false)),
+    /// );
+    /// assert_eq!(
+    ///     AdjectiveStemType::identify("светящееся"),
+    ///     Some(("светящ", AdjectiveStemType::Type4, true)),
+    /// );
+    /// // TODO: should -ся be allowed for non-type-4 words?
+    /// assert_eq!(
+    ///     AdjectiveStemType::identify("красныйся"),
+    ///     Some(("красн", AdjectiveStemType::Type1, true)),
+    /// );
+    /// // TODO: there are no adjectives with stem type 7 though...
+    /// assert_eq!(
+    ///     AdjectiveStemType::identify("ниий"),
+    ///     Some(("ни", AdjectiveStemType::Type7, false)),
+    /// );
+    ///
+    /// assert_eq!(AdjectiveStemType::identify("wxyz"), None);
+    /// assert_eq!(AdjectiveStemType::identify("ы"), None);
+    /// assert_eq!(AdjectiveStemType::identify(""), None);
+    /// ```
     pub const fn identify(word: &str) -> Option<(&str, AdjectiveStemType, bool)> {
         let (word, is_reflexive) = {
             // Remove 'ся' suffix from reflexive adjectives
